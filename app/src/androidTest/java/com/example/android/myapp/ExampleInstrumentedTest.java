8af8e1fb83b16dd5d25ea51187736a7a50eeb9ed
SultanHedfors <35869410+SultanHedfors@users.noreply.github.com>
2018-03-15 13:43:03 +0100

    Update ExampleInstrumentedTest.java

--- app/src/androidTest/java/com/example/android/myapp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/android/myapp/ExampleInstrumentedTest.java
@@ -3,10 +3,8 @@ package com.example.android.myapp;
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
 import static org.junit.Assert.assertEquals;
 
 /**
@@ -20,7 +18,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() throws Exception {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
-
         assertEquals("com.example.android.myapp", appContext.getPackageName());
     }
 }
